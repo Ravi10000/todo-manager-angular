64429c448e96f5b62064ea21cbf6bd58d5510f23
unknown <ravisince2k@gmail.com>
2024-08-22 10:58:50 +0530

    add toggle todo

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,63 +1,70 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from '../../interfaces/todo.interface';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 import { TodoModalComponent } from '../todo-modal/todo-modal.component';
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
 interface TodoListResponse {
-  status: string,
-  message: string,
-  todos: Todo[]
+  status: string;
+  message: string;
+  todos: Todo[];
 }
 interface TodoUpdateResponse {
-  status: string,
-  message: string,
-  todo: Todo
+  status: string;
+  message: string;
+  todo: Todo;
 }
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [NgFor, NgIf, TodoModalComponent],
+  imports: [NgFor, NgIf, NgClass, TodoModalComponent],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrl: './todo-list.component.css',
 })
 export class TodoListComponent {
-  readonly ROOT_URL = "http://localhost:3040/api";
+  readonly ROOT_URL = 'http://localhost:3040/api';
   todos: Todo[] = [];
   isModalOpen = false;
-  selectedTodo:Todo = {name: "", description: ""};
+  selectedTodo: Todo = { name: '', description: '' };
 
   constructor(private http: HttpClient) {}
-  ngOnInit(){
+  ngOnInit() {
     this.http.get<TodoListResponse>(`${this.ROOT_URL}/todos`).subscribe(
-      (response) => { 
+      (response) => {
         this.todos = response.todos;
-        console.log(this.todos) },
-      (error) => { console.log(error); });
-    };
-  openModal(){
+        console.log(this.todos);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  openModal() {
     this.isModalOpen = true;
   }
-  closeModal(){
+  closeModal() {
     this.isModalOpen = false;
-    this.selectedTodo = {name: "", description: ""};
+    this.selectedTodo = { name: '', description: '' };
   }
-  setSelectedTodo(todo:Todo){
+  setSelectedTodo(todo: Todo) {
     this.selectedTodo = todo;
   }
-  toggleTodo(todo:Todo, index: number){
-    console.log("toggle", todo, index);
-    this.http.put<TodoUpdateResponse>(`${this.ROOT_URL}/todos/${todo._id}`, {completed: !todo.completed}).subscribe(response => {
-      console.log({response});
-      this.todos[index] = response.todo;
-      console.log({todos: this.todos});
-      
-    });
+  toggleTodo(todo: Todo, index: number) {
+    console.log('toggle', todo, index);
+    this.todos[index].completed = !todo.completed;
+    this.http
+      .put<TodoUpdateResponse>(`${this.ROOT_URL}/todos/${todo._id}`, {
+        completed: this.todos[index].completed,
+      })
+      .subscribe((response) => {
+        console.log({ response });
+        console.log({ todos: this.todos });
+      });
   }
   // toggleTodo(todo:Todo, index: number){
-    // console.log({todo, index});
-    // this.http.post<TodoUpdateResponse>(`${this.ROOT_URL}/todos/${todo._id}`, {completed: !todo.completed}).pipe(tap(response => {console.log({response})}));
+  // console.log({todo, index});
+  // this.http.post<TodoUpdateResponse>(`${this.ROOT_URL}/todos/${todo._id}`, {completed: !todo.completed}).pipe(tap(response => {console.log({response})}));
   // }
 }
